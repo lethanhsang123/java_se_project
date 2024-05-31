e01449274c90a85f8417a96b4c6f523672b59600
sang <lethanhsang14082001@gmail.com>
2024-06-01 06:56:07 +0700

    [feat][java effective][Builder parttern]

--- src/books/effective_java/item_2_builder_parttern/hierarchies/Pizza.java
+++ src/books/effective_java/item_2_builder_parttern/hierarchies/Pizza.java
@@ -1,4 +1,24 @@
 package books.effective_java.item_2_builder_parttern.hierarchies;
 
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
 public abstract class Pizza {
+    public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
+
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T topping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+        abstract T self();
+    }
+
+    private Pizza(Builder<?> builder) {
+        toppings = builder.toppings;
+    }
 }
